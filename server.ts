c687d688606847dcc30432b569c2854f83582f2e
Abhesh Mandal <abheshmandal249@gmail.com>
2024-10-15 13:41:42 +0545

    Db connection Done

--- server.ts
+++ server.ts
@@ -1,8 +1,10 @@
 import app from "./src/app"
 import { envConfig } from "./src/config/config"
+import connectionToDb from "./src/config/db"
 
 
-function startServer(){
+ function startServer(){
+     connectionToDb()
     const port = envConfig.port || 4000
     app.listen(port,()=>{
         console.log(`Server has statrted at port[${port}]`)
