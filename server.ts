55d66a8097fca89493bb98315b63762ed35872f9
Abhesh Mandal <abheshmandal249@gmail.com>
2024-10-15 13:16:02 +0545

    Basic Setup Node

---
+++ server.ts
@@ -0,0 +1,15 @@
+import app from "./src/app"
+import { envConfig } from "./src/config/config"
+
+
+function startServer(){
+    const port = envConfig.port || 4000
+    app.listen(port,()=>{
+        console.log(`Server has statrted at port[${port}]`)
+
+
+
+
+    })
+}     
+startServer()                            
