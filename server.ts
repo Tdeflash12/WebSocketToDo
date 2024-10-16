a59afe9a4e0f2680b4c59852394dfe35cae56258
Abhesh Mandal <abheshmandal249@gmail.com>
2024-10-16 07:55:47 +0545

    basic learn websocket

--- server.ts
+++ server.ts
@@ -1,17 +1,34 @@
 import app from "./src/app"
 import { envConfig } from "./src/config/config"
 import connectionToDb from "./src/config/db"
-
+import {Server} from "socket.io"
 
  function startServer(){
      connectionToDb()
     const port = envConfig.port || 4000
-    app.listen(port,()=>{
+   const server = app.listen(port,()=>{
         console.log(`Server has statrted at port[${port}]`)
 
 
-
+// post - data receive grdaaa -on
+//get  - data  paudaaa - emit
+//request - socket
+//api -- event
+//req.body=data
 
     })
+    const io = new Server(server)
+    io.on("connection",(socket)=>{
+        socket.on("haha",(data)=>{
+            console.log(data) 
+            socket.emit("abhesh",{
+                message: "Data receive vayo hai tw"
+            })
+        })
+        console.log(socket)
+        
+        console.log("Someone Connected (Customer)")
+    })
 }     
+
 startServer()                            
